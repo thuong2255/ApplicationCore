b03a7d549f6998713cfa986177638fe0342300c7
Nguyễn Văn Thường <thuong2255@gmail.com>
2018-04-15 18:13:03 +0700

    Create Area Admin

--- SystemCoreApp/Startup.cs
+++ SystemCoreApp/Startup.cs
@@ -87,6 +87,10 @@ namespace SystemCoreApp
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                    name: "areaAdmin",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
