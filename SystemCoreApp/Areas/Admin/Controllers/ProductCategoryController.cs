4ac659c537bba18f74819a13dc2e39eb38723f81
Nguyễn Văn Thường <thuong2255@gmail.com>
2018-04-25 19:01:15 +0700

    Hiển thị danh mục sản phẩm dạng TreeView

---
+++ SystemCoreApp/Areas/Admin/Controllers/ProductCategoryController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SystemCore.Service.Interfaces;
+
+namespace SystemCoreApp.Areas.Admin.Controllers
+{
+    public class ProductCategoryController : BaseController
+    {
+        private readonly IProductCategoryService _productCategoryService;
+
+        public ProductCategoryController(IProductCategoryService productCategoryService)
+        {
+            _productCategoryService = productCategoryService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetProductCategory()
+        {
+            var productCategories = _productCategoryService.GetAll();
+            return new OkObjectResult(productCategories);
+        }
+    }
+}
---
+++ SystemCoreApp/wwwroot/app/controllers/productCategory/index.js
@@ -0,0 +1,34 @@
+﻿var productCategory = function() {
+    this.init = function () {
+        loadData();
+    };
+
+    var loadData = function () {
+        var data = [];
+
+        $.ajax({
+            type: 'GET',
+            url: '/admin/productcategory/getproductcategory',
+            dataType: 'json',
+            success: function (response) {
+                response.forEach(function (item) {
+                    data.push({
+                        id: item.Id,
+                        text: item.Name,
+                        parentId: item.ParentId,
+                        sortOrder: item.SortOrder
+                    });
+                });
+
+                var dataTree = common.unflattern(data);
+                $("#tree").tree({
+                    data: dataTree,
+                    dnd: true
+                });
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        });
+    };
+};
